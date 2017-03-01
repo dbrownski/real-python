8ead0e4f94d0a8819568856d63edd6168d94f128
David Brown <dbrown@ermsecurity.com>
2017-03-01 12:01:02 -0500

    flask-hello-world

--- app.py
+++ app.py
@@ -2,11 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
 
 @app.route('/')
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!!??!!!???!!'
+
+
+@app.route('/test/<search_query>')
+def search(search_query):
+    return search_query
+
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "dave":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 
 if __name__ == '__main__':
