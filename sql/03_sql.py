6df074eddd294ebecacadcb0ed5579a6872aecef
David Brown <dbrown@ermsecurity.com>
2017-03-01 21:59:44 -0500

    requirements addition

--- sql/03_sql.py
+++ sql/03_sql.py
@@ -1,14 +1,12 @@
 import sqlite3
 
 with sqlite3.connect("new_db") as connection:
-    cursor = connection.cursor()
+    c = connection.cursor()
 
-cities = [('Rock Hill', 'SC', 1000000),
+cities = (('Rock Hill', 'SC', 1000000),
           ('Boston', 'MA', 600000),
           ('Houston', 'TX', 2100000),
           ('Chicago', 'IL', 2700000),
-          ('Phoenix', 'AZ', 1500000)
+          ('Phoenix', 'AZ', 1500000))
 
-]
-
-cursor.executemany('INSERT INTO population VALUES(?, ?, ?)', cities)
+c.executemany('INSERT INTO population VALUES(?, ?, ?)', cities)
