5e3bdd3cbeedd97aab3d90383e304bf509f297bf
David Brown <dbrown@ermsecurity.com>
2017-03-11 00:51:27 -0500

    flask 105

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -3,6 +3,7 @@
 # imports
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
+from functools import  wraps
 
 import sqlite3
 
@@ -17,6 +18,17 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -27,7 +39,7 @@ def login():
     status_code = 200
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] or \
-            request.form['password'] != app.config['PASSWORD']:
+           request.form['password'] != app.config['PASSWORD']:
             error = 'INVALID Credentials. Please Keep Trying.'
             status_code = 401
         else:
@@ -37,8 +49,13 @@ def login():
 
 
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('SELECT  * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 
 @app.route('/logout')
@@ -46,6 +63,24 @@ def logout():
     session.pop('logged in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
+
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All Fields are required. Please correct and try again")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('insert into posts (title, post) VALUES (?, ?)',
+        [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New Entry was successfully posted!')
+        return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
